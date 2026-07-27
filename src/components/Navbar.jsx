67837bd20e4abd7004a46eed156a5d95c54d40a9
Volts-code <jm6259@nau.edu>
2026-07-26 17:16:48 -0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,15 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
-function Navbar(){
-
-  return(
+function Navbar() {
+  return (
     <nav className="navbar">
-
-      <h2>Books 4 everyone</h2>
-
+      <NavLink className="brand" to="/">Books for Everyone</NavLink>
       <div className="nav-links">
-            <Link to="/">
-            Books
-            </Link>
-
-            <Link to="/contact">
-            Contact
-            </Link>
-
-            <Link to="/signup">
-            Sign In
-            </Link>
-
-            <Link to="/login">
-            Log In
-            </Link>
+        <NavLink to="/" end>Books</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
       </div>
-
     </nav>
   );
-
 }
+
 export default Navbar;
