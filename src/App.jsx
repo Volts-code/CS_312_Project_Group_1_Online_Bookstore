615e6c5201f3323c67a3bb8606b867bd7aee8784
Volts-code <jm6259@nau.edu>
2026-07-26 17:15:30 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,20 @@
-import { useState } from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import BookDetails from "./pages/BookDetails";
 import Books from "./pages/Books";
 import Contact from "./pages/Contact";
-import Signup from './pages/Signup.jsx';
-import Login from './pages/Login.jsx';
 
 function App() {
-  const [ currentUser, setCurrentUser ] = useState({
-    username: "test",
-    preferences: []
-  });
-
   return (
-
     <BrowserRouter>
-
       <Navbar />
-
       <Routes>
-        
-        <Route 
-          path="/signup"
-          element={<Signup />}
-        />
-        
-        <Route 
-          path="/login"
-          element={<Login currentUser={currentUser} setCurrentUser={setCurrentUser}/>}
-        />
-        
-        <Route
-          path="/"
-          element={<Books currentUser={currentUser}/>}
-        />
-          
-        <Route
-          path="/contact"
-          element={<Contact />}
-        />
-      
+        <Route path="/" element={<Books />} />
+        <Route path="/books/:id" element={<BookDetails />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
-    
     </BrowserRouter>
-  
   );
 }
+
 export default App;
